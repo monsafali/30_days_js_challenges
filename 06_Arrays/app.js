e46a82a7d9aa281777c26a021a2511355289b100
monsafali <monsafali272@gmail.com>
2024-07-18 21:42:51 +0500

    array task added

---
+++ 06_Arrays/app.js
@@ -0,0 +1,77 @@
+// Q1
+
+let arr = [1,2,3,4,5]
+// console.log(arr)
+
+// Q2
+// console.log(arr[arr.length -1])
+// console.log(arr[0])
+
+// Q3
+// arr.push(6)
+// console.log(arr)
+
+// Q4
+// arr.pop()
+// console.log(arr)
+
+// Q5
+// arr.shift()
+// console.log(arr)
+
+// Q6
+// arr.unshift(0)
+// console.log(arr)
+
+
+
+
+// Q7
+
+// let data = [2,4,6,8,10,12,14,16,18,20];
+// let cleandata = data.map(number => number * 2);
+// console.log(cleandata);
+
+
+
+// Q8
+// let data = [1,2,3,4,5,6,7,8,9,10]
+// let even = data.filter(num => num %2 == 0)
+// console.log(even)
+
+
+// Q9
+let data = [1,2,3,4,5,6,7,8,9,10]
+
+let sum = data.reduce((res, el) =>(res +el));
+console.log(sum)
+
+
+// Q10
+// for(let i=0; i<data.length; i++){
+//     console.log(data[i])
+// }
+
+// Q11
+
+// data.forEach(Element =>{
+//     console.log(Element)
+// })
+
+
+
+
+// Q12
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+  
+//   console.log(matrix);
+
+
+//   Q13
+
+
+console.log(matrix[1][1])
